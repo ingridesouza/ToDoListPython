bfe0c3ef809f40fba53a5949ba073405611df9b5
Ingride Souza <134303148+ingridesouza@users.noreply.github.com>
2024-02-23 16:46:59 -0300

    Update

--- menu.py
+++ menu.py
@@ -8,13 +8,14 @@ while controleLoop:
           1- Inserir uma tarefa
           2- Visualizar Tarefas
           3- Marcar como concluida
-          4- Exibir Tarefas por prioridade ou status
-          5 - Para Encerrar
+          4- Remover Tarefa
+          5- Exibir Tarefas por prioridade ou status
+          6 - Para Encerrar
           ''')
     escolha = input("Escolha uma das opções: ")
 
     match escolha:
-        case "1":
+        case '1':
             nome = input("Digite o nome da tarefa: ")
             descricao = input("Digite a descrição da tarefa: ")
             prioridade = input("Digite a prioridade da tarefa: ")
@@ -23,14 +24,17 @@ while controleLoop:
             lista_tarefas.append(nova_tarefa)
             print("Tarefa inserida com sucesso!")
             
-        case "2":
+        case '2':
             funcoes.listar_tarefas(lista_tarefas)
         
         case '3':
             tarefa = input('Digite o nome da tarefa que deseja marcar como concluida: ')
             funcoes.marcar_como_concluida(lista_tarefas, tarefa)
-        
+
         case '4':
+            nomeTarefaRemover = input('Digite o nome da tarefa que deseja remover:')
+        
+        case '5':
             print('-----FILTRO DE TAREFAS-----')
             filtroEscolha = input('Escolha uma opção:\n\n'
                       '1- Filtrar a tarefa por categoria\n'
@@ -49,12 +53,13 @@ while controleLoop:
                 case _:
                     print("Opção inválida! Por favor, escolha uma opção válida.")
 
-        case "5":
+        case '6':
             print("Encerrando o programa. Até Logo!")
             controleLoop = False #faz com que o loop while seja interrompido e o programa saia do loop, encerrando a execução.
             break
 
             # controleLoop = Quando é True, o programa continua em execução; quando é False, o programa termina. Isso é uma forma comum de implementar a lógica de menu em programas que precisam de um loop contínuo até que o usuário decida sair.
-
+        case _ :
+            print('Opção Inválida. Tente Novamente.')
 
         
